d221776027b68a50034475babaa95bf2fc9aad13
DoeHetGewoon <66695935+DoeHetGewoon@users.noreply.github.com>
2021-05-23 17:34:19 +0200

    ✨ | Add Database

--- src/bot.js
+++ src/bot.js
@@ -5,6 +5,7 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 client.config = require(`./config`);
 client.embed = require(`./embed`);
+client.db = require(`./database`);
 
 const eventFiles = fs.readdirSync(`./src/events`).filter((f) => f.endsWith(`.js`));
 eventFiles.forEach((file) => {
---
+++ src/database.js
@@ -0,0 +1,3 @@
+const db = require(`quick.db`);
+
+module.exports = db;
